bb274adedd35fcffab694b2c14c5efc31e0b6fc9
smithbyro <smithbyro@myvuw.ac.nz>
2022-06-19 18:35:31 +1200

    update dom to use table

--- MMM-AT-Bus.js
+++ MMM-AT-Bus.js
@@ -37,33 +37,58 @@ Module.register("MMM-AT-Bus",{
 
         var payloadEmpty = true;
 
-        var wrapper = document.createElement("div");
-        if (body.timeArr && body.timeArr.length > 1) {
-            payloadEmpty = false;
+        var wrapper = document.createElement("table");
+
+        // Title and description
+        var tr = document.createElement("tr");
+        var th = document.createElement("th");
+        var text = ""
+        if (this.config.bus) {
+            text = "Next Arrivals for " + this.config.bus + " at stop " + this.config.stopCode;
+        } else {
+            text = "Next Arrivals at stop " + this.config.stopCode;
+        }
+        var text = document.createTextNode(text) ;
+        th.appendChild(text);
+        tr.appendChild(th)
+        wrapper.appendChild(tr);
+
+        // Arrivals with minutes to arrival
+        if (body.timeArr) {
+            if(body.timeArr.length > 1) {
+                payloadEmpty = false;
+            }
             for(let i = 0; i < body.timeArr.length; i++){
                 var busNum = body.timeArr[i].bus;
                 var minutes = body.timeArr[i].time_minutes;
-                var p = document.createElement("p");
+                var tr = document.createElement("tr");
+                var td = document.createElement("td");
                 var text = document.createTextNode(busNum + " | Ariving in: " + minutes + " minutes") ;
-                p.appendChild(text);
-                wrapper.appendChild(p);
+                td.appendChild(text);
+                tr.appendChild(td)
+                wrapper.appendChild(tr);
             }
         }	
 
-        if (body.timeSch && body.timeSch.length > 1) {
-            payloadEmpty = false;
-            wrapper.appendChild(document.createElement("p").appendChild(document.createTextNode("-------------")));
+        // Arrivals with scheduled time
+        if (body.timeSch) {
+            if(body.timeSch.length > 1) {
+                payloadEmpty = false;
+                wrapper.appendChild(document.createElement("p").appendChild(document.createTextNode("-------------")));
+            }
             for(let i = 0; i < body.timeSch.length; i++){
                 var busNum = body.timeSch[i].bus;
                 var time = body.timeSch[i].time;
-                var p = document.createElement("p");
+                var tr = document.createElement("tr");
+                var td = document.createElement("td");
                 var text = document.createTextNode(busNum + " | Scheduled arrival: " + time);
-                p.appendChild(text);
-                wrapper.appendChild(p);
+                td.appendChild(text);
+                tr.appendChild(td)
+                wrapper.appendChild(tr);
             }
-        }	
-
+        }		
         if (payloadEmpty) {
+            wrapper = document.createElement("div");
             wrapper.appendChild(document.createElement("p").appendChild(document.createTextNode("Finding Bus Arrival Times...")))
         }
         return wrapper;
