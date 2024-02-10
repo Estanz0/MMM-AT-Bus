9258b519ef0b3920d1fac454f3ac47250db52f8e
Byron Smith <68315031+Estanz0@users.noreply.github.com>
2024-02-10 22:29:42 +1300

    fix AT, refactor for additional providers

--- MMM-AT-Bus.js
+++ MMM-AT-Bus.js
@@ -1,108 +1,128 @@
-Module.register("MMM-AT-Bus",{
-    // Default module config.
+Module.register("MMM-AT-Bus", {
+	// Default module config.
 	defaults: {
-		bus: '',
-        stopCode: '',
-        forwardLimit: '3600',   // seconds. Ignore bus trips further in the future
-        backLimit: '300',       // seconds. Ignore bus trips further in the past
-        refresh: '10',          // seconds. Refresh rate 
-        key: 'key',
-        stopName: '',           // Populated on first run
-        responseReceived: false
+		// User Config
+		provider: "AT",
+		routeShortNames: ["NX1"],
+		stopCode: "7036",
+		forwardLimitHours: 2, // seconds. Ignore bus trips further in the future
+		refreshIntervalSeconds: 20, // seconds. Refresh rate
+		key: "key",
+
+		// Populated on first run
+		stopName: "",
+		stopId: "",
+		routeIdToShortName: {},
+
+		responseReceived: false
 	},
 
-	start: function() {
-        Log.info("Starting module: " + this.name);
-        console.log("Starting module: " + this.name);
-        var self = this;
+	start: function () {
+		Log.info("Starting module: " + this.name);
+		console.log("Starting module: " + this.name);
+		var self = this;
 
-        let payload = this.config
-		
-        //Do this once first
-        self.sendSocketNotification('START', payload);
+		let payload = this.config;
 
-        //Then every hour
-        setInterval(function() {
-            self.sendSocketNotification('START', payload);
-        }, (+this.config.refresh) * 1000); 
-    },
+		//Do this once first
+		self.sendSocketNotification("START", payload);
+
+		setInterval(function () {
+			self.sendSocketNotification("START", payload);
+		}, +this.config.refreshIntervalSeconds * 1000);
+	},
 
 	// Override dom generator.
-    getDom: function() {
-        Log.log("Updating MMM-AT-Bus DOM.");
+	getDom: function () {
+		Log.log("Updating MMM-AT-Bus DOM.");
 
-        var body = "";
+		var body = "";
 
-        if(this.text != null){
-            body = this.text;
-        }
+		if (this.text != null) {
+			body = this.text;
+		}
 
-        this.config.stopName = body.stopName
+		this.config.stopName = body.stopName;
 
-        var payloadEmpty = true;
+		var payloadEmpty = true;
 
-        var wrapper = document.createElement("table");
+		var wrapper = document.createElement("table");
 
-        // Title and description
-        var tr = document.createElement("tr");
-        var th = document.createElement("th");
-        var text = ""
-        if (this.config.bus) {
-            text = this.config.stopName + " (" + this.config.bus + ")";
-        } else {
-            text = this.config.stopName;
-        }
-        var text = document.createTextNode(text) ;
-        th.appendChild(text);
-        tr.appendChild(th)
-        wrapper.appendChild(tr);
+		// Title and description
+		var tr = document.createElement("tr");
+		var th = document.createElement("th");
+		var text = "";
+		if (this.config.bus) {
+			text = this.config.stopName + " (" + this.config.bus + ")";
+		} else {
+			text = this.config.stopName;
+		}
+		var text = document.createTextNode(text);
+		th.appendChild(text);
+		tr.appendChild(th);
+		wrapper.appendChild(tr);
 
-        // Arrivals with minutes to arrival
-        if (body.timeArr) {
-            if(body.timeArr.length > 1) {
-                payloadEmpty = false;
-            }
-            for(let i = 0; i < body.timeArr.length; i++){
-                var busNum = body.timeArr[i].bus;
-                var minutes = body.timeArr[i].time_minutes;
-                var tr = document.createElement("tr");
-                var td = document.createElement("td");
-                var text = document.createTextNode(busNum + " | Ariving in: " + minutes + " minutes") ;
-                td.appendChild(text);
-                tr.appendChild(td)
-                wrapper.appendChild(tr);
-            }
-        }	
+		// Arrivals with minutes to arrival
+		if (body.inProgressTrips) {
+			if (body.inProgressTrips.length > 1) {
+				payloadEmpty = false;
+			}
+			for (let i = 0; i < body.inProgressTrips.length; i++) {
+				var bus_name = body.inProgressTrips[i].bus_name;
+				var minutes_to_arrival =
+					body.inProgressTrips[i].minutes_to_arrival;
+				var tr = document.createElement("tr");
+				var td = document.createElement("td");
+				var arriving_in_text =
+					minutes_to_arrival > 0
+						? "Ariving in: " + minutes_to_arrival + " minutes"
+						: "Ariving now";
+				var text = document.createTextNode(
+					bus_name + " | " + arriving_in_text
+				);
+				td.appendChild(text);
+				tr.appendChild(td);
+				wrapper.appendChild(tr);
+			}
+		}
 
-        // Arrivals with scheduled time
-        if (body.timeSch) {
-            if(body.timeSch.length > 1) {
-                payloadEmpty = false;
-            }
-            for(let i = 0; i < body.timeSch.length; i++){
-                var busNum = body.timeSch[i].bus;
-                var time = body.timeSch[i].time;
-                var tr = document.createElement("tr");
-                var td = document.createElement("td");
-                var text = document.createTextNode(busNum + " | Scheduled arrival: " + time);
-                td.appendChild(text);
-                tr.appendChild(td)
-                wrapper.appendChild(tr);
-            }
-        }		
-        if (payloadEmpty) {
-            wrapper = document.createElement("div");
-            var text = this.config.responseReceived ? "No Upcoming Arrivals" : "Finding Bus Arrival Times...";
-            wrapper.appendChild(document.createElement("p").appendChild(document.createTextNode(text)))
-        }
-        return wrapper;
-    },
-	socketNotificationReceived: function(notification, payload) {
-        Log.log("MMM-AT-Bus socket received from Node Helper");
-        if(notification == "AT_GETREQUEST_RESULT"){
-            this.text = payload;
-            this.config.responseReceived = true;
-            this.updateDom();
-        }
-    }
+		// Arrivals with scheduled time
+		if (body.scheduledTrips) {
+			if (body.scheduledTrips.length > 1) {
+				payloadEmpty = false;
+			}
+			for (let i = 0; i < body.scheduledTrips.length; i++) {
+				var bus_name = body.scheduledTrips[i].bus_name;
+				var arrival_time = body.scheduledTrips[i].arrival_time;
+				var tr = document.createElement("tr");
+				var td = document.createElement("td");
+				var text = document.createTextNode(
+					bus_name + " | Scheduled arrival: " + arrival_time
+				);
+				td.appendChild(text);
+				tr.appendChild(td);
+				wrapper.appendChild(tr);
+			}
+		}
+		if (payloadEmpty) {
+			wrapper = document.createElement("div");
+			var text = this.config.responseReceived
+				? "No Upcoming Arrivals"
+				: "Finding Bus Arrival Times...";
+			wrapper.appendChild(
+				document
+					.createElement("p")
+					.appendChild(document.createTextNode(text))
+			);
+		}
+		return wrapper;
+	},
+	socketNotificationReceived: function (notification, payload) {
+		Log.log("MMM-AT-Bus socket received from Node Helper");
+		if (notification == "AT_GETREQUEST_RESULT") {
+			this.text = payload;
+			this.config.responseReceived = true;
+			this.updateDom();
+		}
+	}
 });
